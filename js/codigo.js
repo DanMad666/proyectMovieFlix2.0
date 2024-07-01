5255a01ada3f52e0ca463b43fb948aa2433fee7f
DanMad666 <danimartinez666@gmail.com>
2024-06-30 21:33:29 -0300

    se termina estilos de la pagina administrador

--- js/codigo.js
+++ js/codigo.js
@@ -173,3 +173,63 @@ const cargarPeliculas = async (page = 1) => {
 };
 
 document.addEventListener("DOMContentLoaded", () => { cargarPeliculas(1)});
+
+//Validacion de pagina Administrador
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('admPeliculas').addEventListener('submit', function(event){
+        //detenemos el envio del form
+        event.preventDefault();
+
+        //reseteo de valores
+        resetMensajesError();
+
+        //asigancion y validacion de parametros
+        let txtTitulo = document.getElementById('admTit').value.trim();
+        let txtFecEstreno = document.getElementById('admFecEstreno').value.trim();
+        let txtGenero = document.getElementById('admGenero').value.trim();
+        let txtDuracion = document.getElementById('admDuracion').value.trim();
+        let txtDirector = document.getElementById('admDirector').value.trim();
+        let txtReparto = document.getElementById('admReparto').value.trim();
+        let txtSinopsis = document.getElementById('admSinopsis').value.trim();
+        let txtImagen = document.getElementById('admImagen').value;
+        let isValid = true;
+
+        if (txtTitulo === ''){
+            mostrarMensajeError('titulError', 'El campo no puede estar vacío');
+            isValid = false;
+        }
+        if (txtFecEstreno === ''){
+            mostrarMensajeError('fechaestrenoError', 'El campo no puede estar vacio');
+            isValid = false;
+        }
+        if (txtGenero === ''){
+            mostrarMensajeError('generoError', 'El campo no puede estar vacío');
+            isValid = false;
+        }
+        if (txtDuracion === ''){
+            mostrarMensajeError('duracionError', 'El campo no puede estar vacío');
+            isValid = false;
+        }
+        if (txtDirector === ''){
+            mostrarMensajeError('directorError', 'El campo no puede estar vacío');
+            isValid = false;
+        }
+        if (txtReparto === ''){
+            mostrarMensajeError('repartoError', 'El campom no puede estar vacío');
+            isValid = false;
+        }
+        if (txtSinopsis === ''){
+            mostrarMensajeError('sinopsisError', 'El campo no puede estar vacío');
+            isValid = false;
+        }
+        if (txtImagen === ''){
+            mostrarMensajeError('imagenError', 'Debe agregar la imagen de la película');
+            isValid = false;
+        }
+        //si todo está completo, se cargan los datos
+        if (isValid){
+            this.submit();
+            alert('Registro cargado correctamente');
+        }
+    })
+})
